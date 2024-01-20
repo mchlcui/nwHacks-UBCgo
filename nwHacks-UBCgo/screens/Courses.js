a1e74b5c3e0ef76331493a0b13a897ed0f7a0fb2
NACK2 <nicholaskang5@gmail.com>
2024-01-20 13:44:38 -0800

    - removed excess files, moved courses btn to dashboard

--- nwHacks-UBCgo/screens/Courses.js
+++ nwHacks-UBCgo/screens/Courses.js
@@ -1,35 +1,11 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {Text, View } from 'react-native';
 
 const Courses = () => {
     return (
         <View>
             <Text>Balls</Text>
-            <TouchableOpacity style={styles.btnContainer}>
-                <Text style={styles.btnStyle}>
-                    Courses
-                </Text>
-            </TouchableOpacity>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    btnContainer: {
-      elevation: 8,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#3232a8",
-      borderRadius: 10,
-      // paddingVertical: 10,
-      // paddingHorizontal: 12,
-      height: 100,
-      width: 100
-    },
-    btnStyle: {
-      color:"white",
-      fontWeight: "bold",
-      fontSize: 16
-    }
-  });
 
 export default Courses;
