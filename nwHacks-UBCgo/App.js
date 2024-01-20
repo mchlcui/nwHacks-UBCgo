faadd8919dd722a20ec30c55c1bd2e26c59df1fb
NACK2 <nicholaskang5@gmail.com>
2024-01-20 13:41:36 -0800

    -test commit

--- nwHacks-UBCgo/App.js
+++ nwHacks-UBCgo/App.js
@@ -55,21 +55,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  btnContainer: {
-    elevation: 8,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#3232a8",
-    borderRadius: 10,
-    // paddingVertical: 10,
-    // paddingHorizontal: 12,
-    height: 100,
-    width: 100
-  },
-  btnStyle: {
-    color:"white",
-    fontWeight: "bold",
-    fontSize: 16
   }
 });
