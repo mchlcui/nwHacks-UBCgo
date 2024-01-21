3a26cfc2b20ac1bc88b63fc9800985f73f88d07f
alevi29 <aca277@sfu.ca>
2024-01-21 02:14:07 -0800

    added night mode to dining page

--- UBCgo/screens/Dining.js
+++ UBCgo/screens/Dining.js
@@ -1,5 +1,6 @@
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 let POIs = [
     {
@@ -114,7 +115,7 @@ let POIs = [
         title: "The Point",
         location: {
             latitude: 49.262233028531654,
-            longitude:  -123.25552833908395
+            longitude: -123.25552833908395
         },
         description: ""
     },
@@ -123,7 +124,7 @@ let POIs = [
         title: "Gallery Patio & Lounge",
         location: {
             latitude: 49.26698549623312,
-            longitude:  -123.25021370119931
+            longitude: -123.25021370119931
         },
         description: ""
     },
@@ -140,14 +141,261 @@ let POIs = [
     {
         title: "Orchard Commons (Food Court)",
         location: {
-            latitude: 49.260137118748396, 
+            latitude: 49.260137118748396,
             longitude: -123.25053955257721
         },
         description: ""
     }
 ]
 
+const dayStyle = [
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
+
+const nightStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#263c3f"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#6b9a76"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#38414e"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#212a37"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9ca5b3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#1f2835"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#f3d19c"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2f3948"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#17263c"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#515c6d"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#17263c"
+            }
+        ]
+    }
+]
+
 const Dining = () => {
+    const [currentTime, setCurrentTime] = useState('');
+
     const onRegionChange = (region) => {
     }
 
@@ -163,10 +411,18 @@ const Dining = () => {
         })
     }
 
+    useEffect(() => {
+        var hours = new Date().getHours();
+        setCurrentTime(hours);
+        console.log(hours);
+    }, []);
+
     return (
         <View>
             <MapView
+                provider={PROVIDER_GOOGLE}
                 style={styles.map}
+                customMapStyle={currentTime > 18 || currentTime < 6 ? nightStyle : dayStyle}
                 onRegionChange={onRegionChange}
                 initialRegion={{
                     latitude: 49.2606,
@@ -184,7 +440,7 @@ const Dining = () => {
 const styles = StyleSheet.create({
     map: {
         width: '100%',
-        height: '100%'
+        height: '100%',
     }
 })
 
