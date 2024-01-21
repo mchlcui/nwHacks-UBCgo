4c5f32653e79ad23ec5eda09c9d3e49dd73cbf25
NACK2 <nicholaskang5@gmail.com>
2024-01-21 01:10:55 -0800

    Balls

--- UBCgo/screens/Settings.js
+++ UBCgo/screens/Settings.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 const Settings = () => {
     return (
         <View>
-            <Text>Balls</Text>
+            <Text>Blank Settings</Text>
         </View>
     )
 }
